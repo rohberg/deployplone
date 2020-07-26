5232f21aa533aacf2b5ce84af933ab7369213aac
Katja Suess <k.suess@rohberg.ch>
2020-07-26 17:45:16 +0200

    Create component.py

---
+++ components/myapp/component.py
@@ -0,0 +1,18 @@
+from batou.component import Component
+from batou.lib.file import File
+
+
+class Tick(Component):
+
+    def configure(self):
+        self += File(
+            'tick.sh',
+            mode=0755,
+            content="""\
+#!/bin/bash
+while true; do
+  date
+  sleep 1
+done
+""")
+
