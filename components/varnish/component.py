97251018b20aa62f62d8604cecbde2976cd56e1e
Katja Suess <k.suess@rohberg.ch>
2020-07-26 20:43:40 +0200

    varnish

---
+++ components/varnish/component.py
@@ -0,0 +1,53 @@
+from batou import UpdateNeeded
+from batou.component import Component, Attribute
+from batou.lib.cmmi import Build
+from batou.lib.file import File
+from batou.utils import Address
+
+
+class Varnish(Component):
+
+    address = Attribute(Address, '127.0.0.1:9090')
+    control_port = Attribute(int, '6083')
+
+    def configure(self):
+        # self.provide('varnish:http', self)
+        self.purgehosts = self.require('zope:http')
+        self.haproxy = self.require_one('haproxy:frontend')
+
+        # self.cmd('export LC_ALL=en_US.UTF-8; export LANG=en_US.UTF-8; export LANGUAGE=en_US.UTF-8')
+        self += Build(
+            'https://varnish-cache.org/_downloads/varnish-6.4.0.tgz',
+            checksum='sha256:f636ba2d881b146f480fb52efefae468b36c2c3e6620d07460f9ccbe364a76c2',
+        )
+        self += File('zhkath.vcl', source='zhkath.vcl')
+        # self += Program(
+        #     'varnish',
+        #     priority=20,
+        #     command='sbin/varnishd',
+        #     args=self.expand(
+        #         '-F -f {{component.workdir}}/zhkath.vcl '
+        #         '-T localhost:{{component.control_port}} '
+        #         '-a {{component.address.listen}} -p thread_pool_min=10 '
+        #         '-p thread_pool_max=50 -s malloc,250M '
+        #         '-n zhkath'
+        #     )
+        # )
+
+        self += PurgeCache()
+
+
+class PurgeCache(Component):
+
+    varnishadm = 'bin/varnishadm'
+
+    def verify(self):
+        raise UpdateNeeded()
+
+    def update(self):
+        self.cmd(self.expand(
+            '{{component.varnishadm}}'
+            ' -S {{component.workdir}}/var/varnish/zhkath/_.secret'
+            ' -T "localhost:{{component.parent.control_port}}"'
+            ' "ban req.url ~ .*"')
+        )
