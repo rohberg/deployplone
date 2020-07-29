af5257789c1bb20296f168bf087efc2de2118116
Katja Suess <k.suess@rohberg.ch>
2020-07-29 14:47:01 +0200

    Update website.pm2.config.js

--- components/pm2/website.pm2.config.js
+++ components/pm2/website.pm2.config.js
@@ -1,12 +1,26 @@
 module.exports = {
   apps: [
     {
-      script: "{{component.voltoapp.workdir}}/build/server.js",
       name: "{{component.voltoappname}}",
-      cwd: "{{component.voltoapp.workdir}}"
+      script: "{{component.voltoapp.workdir}}/build/server.js",
+      cwd: "{{component.voltoapp.workdir}}",
+      watch       : true,
+      env: {
+        "NODE_ENV": "production",
+      },
+      env_development : {
+         "NODE_ENV": "development"
+      }
     }
   ]
 };
 
+// start with pm2 restart <pathtofile>/website.pm2.config.js
+// or ... --env development to start in development mode
+
+
+
+// see documentation:
+// https://pm2.keymetrics.io/docs/usage/application-declaration/
 
 // TOOD zeo, zope, instances
