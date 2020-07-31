aaecc474abc2e4a6cd1c550fa52612ab8680f16c
Katja Suess <k.suess@rohberg.ch>
2020-07-31 11:53:22 +0200

    create plone default site "Plone" with example content

--- components/voltoapp/component.py
+++ components/voltoapp/component.py
@@ -21,7 +21,7 @@ configuration = {
 class Voltoapp(Component):
     apprepository = Attribute(str, configuration['apprepository'])
     address = Attribute(Address, 'localhost:3000')
-    razzleapipath = Attribute(str, 'http://localhost:11080/Plone/api') # TODO haproxy port eintragen
+    razzleapipath = Attribute(str, 'http://localhost:11080/Plone') # TODO haproxy port eintragen
 
     def configure(self):
         self.provide('voltoapp', self)
