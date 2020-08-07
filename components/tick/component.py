89e5a86afbf94f6fa4bdc2db14baca77e275b05b
Katja Suess <k.suess@rohberg.ch>
2020-08-07 18:53:09 +0200

    test component tick

--- components/tick/component.py
+++ components/tick/component.py
@@ -5,7 +5,7 @@ from batou.lib.file import File
 class Tick(Component):
 
     def configure(self):
-        self.common = self.require_one('common', host=self.host)
+        # self.common = self.require_one('common', host=self.host)
         self += File(
             'tick.sh',
             mode=0o700,
